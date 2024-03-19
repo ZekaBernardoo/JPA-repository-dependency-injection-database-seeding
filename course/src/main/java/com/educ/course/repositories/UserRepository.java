d41153fee2364f5b55668742eee11fd3065e14a2
Zeka Bernardo <zekabernardo@gmail.com>
2024-03-19 16:42:56 -0300

    Criados respositorios

---
+++ course/src/main/java/com/educ/course/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.educ.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.educ.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
